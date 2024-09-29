498de9cb3390d7f800a9ec5a1cc6a6d2f736002d
mihawke <pratikgawari0000@gmail.com>
2024-09-29 21:09:52 +0530

    modified

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom'
 import { FaCircleArrowDown, FaCircleArrowUp } from 'react-icons/fa6';
