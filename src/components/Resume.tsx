a8aa46ab10613676a44b2f3285235a6bce024ba0
mihawke <pratikgawari0000@gmail.com>
2024-11-15 14:45:49 +0530

    updated

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -2,7 +2,7 @@ const Resume = () => {
   return (
     <main className="bg-[#1C1C24] flex flex-col lg:flex-row lg:gap-4 flex-1 items-center p-6 overflow-scroll">
       <object
-        data="/Pratik Gawari Resume.pdf" // Adjust this path based on your project structure
+        data="/Pratik Resume Final.pdf" // Adjust this path based on your project structure
         type="application/pdf"
         width="80%"
         height="100%"
@@ -12,7 +12,7 @@ const Resume = () => {
         </p>
       </object>
       <a
-        href="/Pratik Gawari Resume.pdf"
+        href="/Pratik Resume Final.pdf"
         download="Pratik Gawari Resume.pdf" // Filename to be downloaded
         className="w-fit px-4 py-2 bg-[#E94560] text-white text-sm md:text-md font-semibold rounded-md shadow-md hover:bg-[#D73752] transition duration-200 font-source_code_pro_bold"
       >Download Resume</a>
